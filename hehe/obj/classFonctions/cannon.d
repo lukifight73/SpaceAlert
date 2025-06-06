5061e5d38eb2f3d6bb8b855bdbf454adf2c12885
Lucas Benatar <lbenatar@c4r2s3.clusters.forty2.fr>
2025-06-06 19:20:44 +0200

    push sans -o

--- hehe/obj/classFonctions/cannon.d
+++
@@ -1,6 +0,0 @@
-obj/classFonctions/cannon.o: src/classFonctions/cannon.cpp \
-  include/../include/cannon.hpp include/space_alerte_values.hpp
-
-include/../include/cannon.hpp:
-
-include/space_alerte_values.hpp:
--- hehe/obj/classFonctions/carte_action_hero.d
+++
@@ -1,2 +0,0 @@
-obj/classFonctions/carte_action_hero.o: \
-  src/classFonctions/carte_action_hero.cpp
